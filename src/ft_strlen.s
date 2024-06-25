3b65102cc0a585f5c4ac1ccbf0339c9e7a94e7eb
Raoni Lacroux <81537154+LacrouxRaoni@users.noreply.github.com>
2024-06-25 18:23:07 -0300

    Atualizar o ft_strlen.s
    comments added in the script

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,15 +1,15 @@
 section .text
-global ft_strlen
+global ft_strlen            ;define the function ft_strlen can be used in other modules. 
 
 ft_strlen:
-    xor rcx, rcx
+    xor rcx, rcx            ;set the rcx to 0 using operand xor by bitwise
 
 _loop:
-    cmp byte [rdi + rcx], 0
-    je done
-    inc rcx
-    jmp _loop
+    cmp byte [rdi + rcx], 0 ;checks if the current character is the end of the string (\0)
+    je done                 ;jump to done label when cmp find the end byte
+    inc rcx                 ;increment rcx by 1
+    jmp _loop               ;jump to the _loop label executing the loop again.
 
 done:
-    mov rax, rcx
-    ret
+    mov rax, rcx            ;move the rcx register value to rax
+    ret                     ;return size len
